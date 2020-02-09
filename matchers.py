1d937967ab7b62a111d51b6eb117536215b01321
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2020-02-10 00:52:59 +0100

    Added simple matcher for Polish bank account numbers

--- matchers.py
+++ matchers.py
@@ -2,13 +2,13 @@ import spacy
 import re
 
 
-def NIPmatcher(doc: spacy.tokens.Doc) -> str:
+def NIP_matcher(doc: spacy.tokens.Doc) -> str:
     """
-    Finds instances of Polish VAT ID (NIP) in the text and returns the annotated string
+    Finds instances of Polish VAT ID (NIP) in the document and returns the annotated text
     :param doc: input document
     :return: string with VAT ID number enclosed in <NIP> tags
     """
-    expression = r"((PL)*)(\d{10}|\d{3}-\d{3}-\d{2}-\d{2}|\d{3}-\d{2}-\d{2}-\d{3})"
+    expression = r"((PL)*)(\d{10}|\d{3}-\d{3}-\d{2}-\d{2}|\d{3}-\d{2}-\d{2}-\d{3})[a-zA-Z]*"
 
     result = doc.text
 
@@ -18,5 +18,26 @@ def NIPmatcher(doc: spacy.tokens.Doc) -> str:
 
         if span:
             result = result.replace(doc.text[start:end], f'<NIP>{doc.text[start:end]}</NIP>')
+
+    return result
+
+
+def bank_number_matcher(doc: spacy.tokens.Doc) -> str:
+    """
+    Finds instances of bank account numbers in the document and returns the annotated text
+
+    :param doc: input document
+    :return: string with bank account numbers enclosed in <BANK_NO> tags
+    """
+    expression = r"((PL)*)(\d{2} (\d{4}\s*){6}|\d{2} \d{24}|\d{26})[a-zA-Z]*"
+
+    result = doc.text
+
+    for match in re.finditer(expression, doc.text):
+        start, end = match.span()
+        span = doc.char_span(start, end)
+
+        if span:
+            result = result.replace(doc.text[start:end], f'<BANK_NO>{doc.text[start:end]}</BANK_NO>')
 
     return result
--- unit_tests/matchers_test.py
+++ unit_tests/matchers_test.py
@@ -1,9 +1,9 @@
 import unittest
 import spacy
-from ..matchers import NIPmatcher
+from ..matchers import NIP_matcher, bank_number_matcher
 
 
-class MyTestCase(unittest.TestCase):
+class MatchersTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -11,31 +11,47 @@ class MyTestCase(unittest.TestCase):
 
     def test_NIP_no_dashes(self):
         doc = self.nlp('numer NIP 1234567890')
-        self.assertTrue('<NIP>' in NIPmatcher(doc))
+        self.assertTrue('<NIP>' in NIP_matcher(doc))
 
     def test_NIP_dashes_pattern_3322(self):
         doc = self.nlp('numer NIP 123-456-78-90')
-        self.assertTrue('<NIP>' in NIPmatcher(doc))
+        self.assertTrue('<NIP>' in NIP_matcher(doc))
 
     def test_NIP_dashes_pattern_3223(self):
         doc = self.nlp('numer NIP 123-45-67-890')
-        self.assertTrue('<NIP>' in NIPmatcher(doc))
+        self.assertTrue('<NIP>' in NIP_matcher(doc))
 
     def test_NIP_with_country_prefix(self):
         doc = self.nlp('numer NIP PL 1234567890')
-        self.assertTrue('<NIP>' in NIPmatcher(doc))
+        self.assertTrue('<NIP>' in NIP_matcher(doc))
 
     def test_invalid_NIP(self):
         doc = self.nlp('numer NIP 12-34-56-78-90')
-        self.assertFalse('<NIP>' in NIPmatcher(doc))
+        self.assertFalse('<NIP>' in NIP_matcher(doc))
 
     def test_no_valid_NIP(self):
         doc = self.nlp('kod pocztowy 62-090')
-        self.assertFalse('<NIP>' in NIPmatcher(doc))
+        self.assertFalse('<NIP>' in NIP_matcher(doc))
+
+    def test_NIP_concatenated_with_text(self):
+        doc = self.nlp('numer NIP 1234567890dodatkowytekst')
+        self.assertTrue('<NIP>' in NIP_matcher(doc))
 
     def test_two_NIPs(self):
         doc = self.nlp('NIP sprzedawcy 123-45-67-890 i NIP klienta 0987654321')
-        self.assertTrue(NIPmatcher(doc).count('<NIP>') == 2)
+        self.assertTrue(NIP_matcher(doc).count('<NIP>') == 2)
+
+    def test_bank_account_spaces(self):
+        doc = self.nlp('BRE BANK: 12 1234 1234 1234 1234 1234 1234')
+        self.assertTrue('<BANK_NO>' in bank_number_matcher(doc))
+
+    def test_bank_account_no_spaces(self):
+        doc = self.nlp('BRE BANK: 12123412341234123412341234')
+        self.assertTrue('<BANK_NO>' in bank_number_matcher(doc))
+
+    def test_bank_account_concatenated_with_text(self):
+        doc = self.nlp('BRE BANK: 12123412341234123412341234dodatkowytekst')
+        self.assertTrue('<BANK_NO>' in bank_number_matcher(doc))
 
 
 if __name__ == '__main__':
