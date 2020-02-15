0d469e6d3b5b498ec8eeb53769daea3b554e875d
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2020-02-15 23:46:56 +0100

    Refactoring taggers and renaming source files

--- unit_tests/taggers_test.py
+++ unit_tests/taggers_test.py
@@ -1,7 +1,6 @@
 import unittest
 import spacy
-from ..matchers import NIPmatcher, date_tagger
-from ..matchers import NIP_matcher, bank_number_matcher
+from ..taggers import nip_tagger, bank_number_tagger, date_tagger, regon_tagger
 
 
 class MatchersTestCase(unittest.TestCase):
@@ -12,47 +11,47 @@ class MatchersTestCase(unittest.TestCase):
 
     def test_NIP_no_dashes(self):
         doc = self.nlp('numer NIP 1234567890')
-        self.assertTrue('<NIP>' in NIP_matcher(doc).text)
+        self.assertTrue('<NIP>' in nip_tagger(doc).text)
 
     def test_NIP_dashes_pattern_3322(self):
         doc = self.nlp('numer NIP 123-456-78-90')
-        self.assertTrue('<NIP>' in NIP_matcher(doc).text)
+        self.assertTrue('<NIP>' in nip_tagger(doc).text)
 
     def test_NIP_dashes_pattern_3223(self):
         doc = self.nlp('numer NIP 123-45-67-890')
-        self.assertTrue('<NIP>' in NIP_matcher(doc).text)
+        self.assertTrue('<NIP>' in nip_tagger(doc).text)
 
     def test_NIP_with_country_prefix(self):
         doc = self.nlp('numer NIP PL 1234567890')
-        self.assertTrue('<NIP>' in NIP_matcher(doc).text)
+        self.assertTrue('<NIP>' in nip_tagger(doc).text)
 
     def test_invalid_NIP(self):
         doc = self.nlp('numer NIP 12-34-56-78-90')
-        self.assertFalse('<NIP>' in NIP_matcher(doc).text)
+        self.assertFalse('<NIP>' in nip_tagger(doc).text)
 
     def test_no_valid_NIP(self):
         doc = self.nlp('kod pocztowy 62-090')
-        self.assertFalse('<NIP>' in NIP_matcher(doc).text)
+        self.assertFalse('<NIP>' in nip_tagger(doc).text)
 
     def test_NIP_concatenated_with_text(self):
         doc = self.nlp('numer NIP 1234567890dodatkowytekst')
-        self.assertTrue('<NIP>' in NIP_matcher(doc).text)
+        self.assertTrue('<NIP>' in nip_tagger(doc).text)
 
     def test_two_NIPs(self):
         doc = self.nlp('NIP sprzedawcy 123-45-67-890 i NIP klienta 0987654321')
-        self.assertTrue(NIP_matcher(doc).text.count('<NIP>') == 2)
+        self.assertTrue(nip_tagger(doc).text.count('<NIP>') == 2)
 
     def test_bank_account_spaces(self):
         doc = self.nlp('BRE BANK: 12 1234 1234 1234 1234 1234 1234')
-        self.assertTrue('<BANK_NO>' in bank_number_matcher(doc).text)
+        self.assertTrue('<BANK_NO>' in bank_number_tagger(doc).text)
 
     def test_bank_account_no_spaces(self):
         doc = self.nlp('BRE BANK: 12123412341234123412341234')
-        self.assertTrue('<BANK_NO>' in bank_number_matcher(doc).text)
+        self.assertTrue('<BANK_NO>' in bank_number_tagger(doc).text)
 
     def test_bank_account_concatenated_with_text(self):
         doc = self.nlp('BRE BANK: 12123412341234123412341234dodatkowytekst')
-        self.assertTrue('<BANK_NO>' in bank_number_matcher(doc).text)
+        self.assertTrue('<BANK_NO>' in bank_number_tagger(doc).text)
 
     def test_simple_date_with_dashes(self):
         doc = self.nlp('data sprzedaży 12-10-2017')
@@ -69,6 +68,10 @@ class MatchersTestCase(unittest.TestCase):
     def test_simple_date_reversed(self):
         doc = self.nlp('data sprzedaży 2020 1 1')
         self.assertTrue('<DATE>' in date_tagger(doc).text)
+
+    def test_simple_REGON(self):
+        doc = self.nlp('Fiat Polska NIP: 123-456-77-89 REGON: 123456789')
+        self.assertTrue('<REGON>' in regon_tagger(doc).text)
 
 
 if __name__ == '__main__':
