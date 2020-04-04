5990c66f59a2df3f1167e7fc45e83ab2e38e9175
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2020-04-04 21:09:08 +0200

    Re-writing bank account matcher as a class for easy pipelining of documents

--- matchers.py
+++ matchers.py
@@ -31,3 +31,34 @@ class NIPMatcher():
             spans.append(Span(doc, start, end, label="NIP"))
         doc.ents = list(doc.ents) + spans
         return doc
+
+
+class BankNumberMatcher():
+    """Extracts matched bank account numbers and adds them as document entities with the label BANK_ACCOUNT_NO"""
+
+    def __init__(self, nlp):
+        """Creates a new bank number account matcher using a shared vocabulary object"""
+        self.matcher = Matcher(nlp.vocab)
+        patterns = [
+            # an optional 2-letter country code followed by 26 consecutive digits
+            [{'TEXT': {'REGEX': '([a-zA-Z][a-zA-Z])?\d{26}'}}],
+            # 26 consecutive digits followed by an optional 2-letter country code
+            [{'TEXT': {'REGEX': '(\d{26}[a-zA-Z][a-zA-Z])?'}}],
+            # an optional 2-letter country code followed by 2-4-4-4-4-4-4 digit pattern (either dashes or spaces)
+            [{'TEXT': {'REGEX': '([a-zA-Z][a-zA-Z])?\d{2}(-|\s)(\d{4}(-|\s)){5}\d{4}'}}],
+            # an optional 2-letter country code followed by 2-24 digit pattern (either dashes or spaces)
+            [{'TEXT': {'REGEX': '([a-zA-Z][a-zA-Z])?\d{2}(-|\s)\d{24}'}}],
+            # 2-4-4-4-4-4-4 digit pattern (either dashes or spaces) followed by an optional 2-letter country code
+            [{'TEXT': {'REGEX': '\d{2}(-|\s)(\d{4}(-|\s)){5}\d{4}([\sa-zA-Z][a-zA-Z])?'}}],
+            # 2-24 digit pattern (either dashes or spaces) followed by an optional 2-letter country code
+            [{'TEXT': {'REGEX': '\d{2}(-|\s)\d{24}([\sa-zA-Z][a-zA-Z])?'}}]
+        ]
+        self.matcher.add('BANK_ACCOUNT_NO', None, *patterns)
+
+    def __call__(self, doc: Doc) -> Doc:
+        matches = self.matcher(doc)
+        spans = []
+        for match_id, start, end in matches:
+            spans.append(Span(doc, start, end, label="BANK_ACCOUNT_NO"))
+        doc.ents = list(doc.ents) + spans
+        return doc
--- unit_tests/matchers_test.py
+++ unit_tests/matchers_test.py
@@ -1,7 +1,7 @@
 import unittest
 import spacy
 
-from ..matchers import NIPMatcher
+from ..matchers import NIPMatcher, BankNumberMatcher
 
 
 class MatchersTestCase(unittest.TestCase):
@@ -9,8 +9,6 @@ class MatchersTestCase(unittest.TestCase):
     def __init__(self,  *args, **kwargs):
         super(MatchersTestCase, self).__init__(*args, **kwargs)
         self.nlp = spacy.load('en_core_web_sm')
-        self.nip_matcher = NIPMatcher(self.nlp)
-        self.nlp.add_pipe(self.nip_matcher, before='ner')
 
     def test_NIP(self):
         test_strings = [
@@ -33,9 +31,22 @@ class MatchersTestCase(unittest.TestCase):
             'NIP: 123 45 67 890PL',
         ]
 
+        self.nlp.add_pipe(NIPMatcher(self.nlp), before='ner')
         for test_string in test_strings:
             doc = self.nlp(test_string)
             self.assertTrue('NIP' in [e.label_ for e in doc.ents])
+
+    def test_bank_account_numbers(self):
+        test_strings = [
+            'mbank: 20 123456789012345678901234',
+            'mbank: PL 20 1234 1234 1234 1234 1234 1234',
+            'mbank: 20-1234-1234-1234-1234-1234-1234PL'
+        ]
+
+        self.nlp.add_pipe(BankNumberMatcher(self.nlp), before='ner')
+        for test_string in test_strings:
+            doc = self.nlp(test_string)
+            self.assertTrue('BANK_ACCOUNT_NO' in [e.label_ for e in doc.ents])
 
 
 if __name__ == '__main__':
