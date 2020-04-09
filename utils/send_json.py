cfe29778f34237bd3237dd015b082c554f2edfb2
Oliver Pieńkos <34404522+oliver-pienkos@users.noreply.github.com>
2020-04-09 11:14:59 +0200

    Update send_json.py

--- utils/send_json.py
+++ utils/send_json.py
@@ -3,8 +3,8 @@ from typing import Dict
 
 
 def send(data: Dict, job_id: str, file_id: str) -> object:
-    # Takes in a dictionary, appends job and file id and  converts into json and sends to url.
-    # Returns return code from the site.
+    # Takes in a dictionary, appends job and file id and  converts into json then sends to url.
+    # Returns return code from the site in form of request object.
 
     url = "https://api-smart-invoice-dev.b-mind.pl/api/v1/webhook/ml/send"
     headers = {"X-ML-Signature": "QpachX9AAhKD8CB3USMk5nrMee8QvdLvYLGdhFdDpAr2vyzkK5t3q3T9wbxstMXG"}
