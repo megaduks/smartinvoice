342d35c50729f31ba4ee4f3e6f3d26ab022e44fc
Oliver Pieńkos <oliver.pienkos@gmail.com>
2020-04-09 19:03:48 +0200

    Added the settings.py.

--- utils/send_json.py
+++ utils/send_json.py
@@ -1,20 +1,16 @@
 import requests
-import os
 from typing import Dict
-from dotenv import load_dotenv
+from settings import UPLOAD_URL, ML_SIGNATURE
 
 
 def send(data: Dict, job_id: str, file_id: str) -> object:
     # Takes in a dictionary, appends job and file id and  converts into json and sends to url.
-    # Returns return code from the site.
-    load_dotenv()
-    url = os.getenv("UPLOAD_URL")
-    signature = os.getenv("ML_SIGNATURE")
-    headers = {"X-ML-Signature": signature}
+    # Returns a request object with the return code and data.
 
+    headers = {"X-ML-Signature": ML_SIGNATURE}
     data['job_id'] = job_id
     data['file_id'] = file_id
 
-    response = requests.request(method='POST', url=url, json=data, headers=headers)
+    response = requests.request(method='POST', url=UPLOAD_URL, json=data, headers=headers)
 
     return response
