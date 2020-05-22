13b8a692dca34ed5171e8d03055f1e5ab7a294e1
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2020-05-22 18:35:26 +0200

    Small fixes to tokenizers_test.py, verification of date tokenization

--- unit_tests/tokenizers_test.py
+++ unit_tests/tokenizers_test.py
@@ -3,6 +3,7 @@ import spacy
 
 from tokenizers import create_custom_tokenizer
 
+
 class TokenizersTestCase(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
@@ -14,14 +15,18 @@ class TokenizersTestCase(unittest.TestCase):
                 'FAKTURA nr 5121/H0525', ['FAKTURA', 'nr', '5121', '/', 'H0525']
             ),
             (
-                'Faktura Vat nr: 1965/2019/08/TCFK', ['Faktura', 'Vat', 'nr', ':', '1965', '/', '2019', '/', '08', '/', 'TCFK']
+                'Faktura Vat nr: 1965/2019/08/TCFK',
+                ['Faktura', 'Vat', 'nr', ':', '1965', '/', '2019', '/', '08', '/', 'TCFK']
+            ),
+            (
+                '2020-01-01', ['2020', '-', '01', '-', '01']
             )
         ]
-        nlp = spacy.load('en_core_web_sm')
+        nlp = spacy.load('pl_model')
         nlp.tokenizer = create_custom_tokenizer(nlp)
 
-        for input, output in test_strings:
-            self.assertCountEqual(map(str, nlp(input)), output)
+        for _input, _output in test_strings:
+            self.assertCountEqual(map(str, nlp(_input)), _output)
 
 
 if __name__ == '__main__':
