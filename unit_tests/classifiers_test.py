e856c8c81f62d97f0d4502462b9b7b9c99b18bd7
Mikołaj Morzy <mikolaj.morzy@gmail.com>
2020-08-02 15:14:33 +0200

    Fixed absolute image path issue for Ludwig classifier

--- unit_tests/classifiers_test.py
+++ unit_tests/classifiers_test.py
@@ -1,7 +1,8 @@
 import unittest
 
-from classifiers import *
+from classifiers import InvoicePhotoClassifier, process_invoice
 from settings import INVOICE_IMAGE_MODEL
+from pathlib import Path
 
 
 class ClassifiersTestCase(unittest.TestCase):
@@ -12,13 +13,34 @@ class ClassifiersTestCase(unittest.TestCase):
         self.model = InvoicePhotoClassifier(INVOICE_IMAGE_MODEL)
 
     def test_invoice_image_classifier(self):
-        puppy_image = Path('/data/test/puppy.jpg')
-        book_image = Path('/data/test/book.jpeg')
-        invoice_image = Path('/data/test/invoice.jpeg')
+        # unfortunately, file paths must be absolute for Ludwig classifier
+        project_root = Path('/home/mikolaj/Research/smartinvoice')
+        puppy_image = project_root / 'data/test/puppy.jpg'
+        book_image = project_root / 'data/test/book.jpeg'
+        invoice_image = project_root / 'data/test/invoice.jpeg'
 
         self.assertFalse(self.model.predict(puppy_image))
         self.assertFalse(self.model.predict(book_image))
         self.assertTrue(self.model.predict(invoice_image))
+
+    def test_invoice_processing(self):
+        ocr_file = Path('data/test/faktura.txt')
+        result = process_invoice(ocr_file)
+
+        expected = [
+            {'numer_faktury': '01/07/2020'},
+            {'typ_faktury': 'Faktura VAT'},
+            {'nazwa_nabywcy': 'Baiga Mikołaj Morzy'},
+            {'data_wystawienia': '10/08/2020'},
+            {'miara': 'Szt'},
+            {'stawka_podatku': '23%'},
+            {'forma_platnosci': 'przelew'},
+            {'razem_kwota_brutto': '6115,00'},
+            {'numer_konta_sprzedawcy': '10 1234 5678 1234 5678 1234 5678'},
+        ]
+
+        for e in expected:
+            self.assertTrue(e in result)
 
 
 if __name__ == '__main__':
