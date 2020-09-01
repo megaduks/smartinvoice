dfe989d6c42051f0b706b09e50a528eabe6cf8cc
Oliver Pieńkos <oliver.pienkos@gmail.com>
2020-09-01 14:04:18 +0200

    Minor cosmetic changes :lipstick:

--- queue_handling.py
+++ queue_handling.py
@@ -1,30 +1,32 @@
-import pika
+
 import json
 import spacy
 import cv2
 
 from settings import RABBITMQ_PASSWORD, RABBITMQ_LOGIN, RABBITMQ_SERVER, RABBITMQ_EXCHANGE_NAME
-from net_tools import sendJSON, getImageFromToken
+from net_tools import send_json, get_image_from_token
 from image_processing import InvoiceOCR
 from classifiers import InvoiceNERClassifier
 from typing import Dict, List
+from pika import ConnectionParameters, PlainCredentials, BlockingConnection
 
-credentials = pika.PlainCredentials(RABBITMQ_LOGIN, RABBITMQ_PASSWORD)
-parameters = pika.ConnectionParameters(host=RABBITMQ_SERVER, port=5672, virtual_host="/", credentials=credentials)
+credentials = PlainCredentials(RABBITMQ_LOGIN, RABBITMQ_PASSWORD)
+connection_parameters = ConnectionParameters(host=RABBITMQ_SERVER, port=5672, virtual_host="/", credentials=credentials)
 exchange_name = RABBITMQ_EXCHANGE_NAME
 model = "/home/oliver/Documents/smartinvoice/experimental/prodigy/invoice_model_final/"
 
 
 def processResponse(response, method_frame, OCR, NER):
+
     data = json.loads(response.decode('utf-8'))
     print(f"Received message : {data}")
-    print("[INFO] Downloading image. ")
-    image = getImageFromToken(data["download_token"])
+    print("[INFO] Downloading image.")
+    image = get_image_from_token(data["download_token"])
     ner_results = []
 
     # save image if download succeeds
     if image is not None:
-        filename = f"{method_frame.delivery_tag}.jpg"
+        filename = f"{method_frame.delivery_tag}.png"
         cv2.imwrite(filename, image)
 
         print("[INFO] Processing image")
@@ -43,7 +45,7 @@ def processResponse(response, method_frame, OCR, NER):
     print(payload)
 
     print("[INFO] Sending JSON")
-    sendJSON(payload=payload, job_id=data['job_id'], file_id=data['file_id'])
+    send_json(payload=payload, job_id=data['job_id'], file_id=data['file_id'])
 
 
 def update_payload(payload: Dict) -> Dict:
@@ -71,8 +73,7 @@ def update_payload(payload: Dict) -> Dict:
         if key in keys_tax:
             payload["rozliczenie_vat"][0][key] = payload[key]
 
-    for key in payload.keys():
-        if key in keys_product:
+        elif key in keys_product:
             payload["rozliczenie_vat"][0][key] = payload[key]
 
     if "numer_faktury" not in payload.keys():
@@ -96,10 +97,10 @@ def clean_payload(payload: Dict, entities_to_remove: List) -> Dict:
     return payload
 
 
-def test_connection(parameters):
+def test_connection(parameters: ConnectionParameters):
     # try to establish connection and check its status
     try:
-        connection = pika.BlockingConnection(parameters)
+        connection = BlockingConnection(parameters)
         if connection.is_open:
             print('OK')
             connection.close()
@@ -109,8 +110,8 @@ def test_connection(parameters):
         exit(1)
 
 
-def start_connection(parameters):
-    connection = pika.BlockingConnection(parameters)
+def start_connection(parameters: ConnectionParameters) -> (BlockingConnection, BlockingConnection):
+    connection = BlockingConnection(parameters)
     channel = connection.channel()
     return connection, channel
 
@@ -119,13 +120,13 @@ def callback(channel, method_frame, properties, body, OCR):
     pass
 
 
-def get_queue_info(parameters):
+def get_queue_info(parameters: ConnectionParameters):
     _, channel = start_connection(parameters)
     print(channel.queue_declare(queue="smart-invoice", passive=True))
     channel.close()
 
 
-def start_consuming(parameters, OCR, NLP):
+def start_consuming(parameters: ConnectionParameters, OCR: InvoiceOCR, NLP: InvoiceNERClassifier):
     connection, channel = start_connection(parameters)
     for method_frame, properties, body in channel.consume("smart-invoice"):
 
@@ -143,8 +144,8 @@ def start_consuming(parameters, OCR, NLP):
 
 
 if __name__ == '__main__':
-    get_queue_info(parameters)
+    get_queue_info(connection_parameters)
     nlp = spacy.load(model)
     clf = InvoiceNERClassifier(nlp=nlp)
     OCR = InvoiceOCR(model_path="/home/oliver/Documents/smartinvoice/models/frozen_east_text_detection.pb")
-    start_consuming(parameters, OCR, clf)
+    start_consuming(connection_parameters, OCR, clf)
